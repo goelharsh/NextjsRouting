ec6af1693a24d70dfc7200f60972fae4250ee2ae
napharsh <harshgoel18122001@gmail.com>
2024-10-18 22:59:46 +0530

    Redirection concept

---
+++ src/app/profileData/page.js
@@ -0,0 +1,12 @@
+import { redirect } from "next/navigation";
+
+export default function ProfileData(){
+    // as the data is null so profile data page will automatically redirect to the profile page 
+    const profileData = null;
+    if(profileData===null){
+        redirect('profile');
+    }
+    return (
+        <h1>ProfileData page</h1>
+    )
+}
