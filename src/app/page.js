1fd57c7ab561bfcda88778de6c866a72d213a186
napharsh <harshgoel18122001@gmail.com>
2024-10-18 22:52:09 +0530

    Navigate from one page to another

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,28 @@
-import Image from "next/image";
+'use client'
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  // we should use this hook when we are navigating again and again  back and forward
+  const router = useRouter();
+  console.log(router);
+
+  
+  function handleNavigation(){
+    router.push('products');
+  }
+
   return (
-   <h1>Home</h1>
+  <div>
+     <h1>Home</h1>
+     <Link href="/products">Navigate to products page</Link>
+
+     {/* alternate way to navigate  */}
+     <h2 className="font-bold mt-3 text-lg">Alternative way of navigating using useRouter</h2>
+     <button onClick={handleNavigation}>Navigate using useRouter</button>
+     
+  </div>
   );
 }
